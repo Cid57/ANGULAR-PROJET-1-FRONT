d0bc32776b06df62c7412981331527d108fe9cb9
Cindy Singer <cindy_singer913@msn.com>
2024-04-30 08:31:52 +0200

    modif

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, MatCardModule],
   templateUrl: './accueil.component.html',
-  styleUrl: './accueil.component.scss'
+  styleUrl: './accueil.component.scss',
 })
 export class AccueilComponent {
+  http: HttpClient = inject(HttpClient);
+  listeProduit: any = [];
 
+  ngOnInit() {
+    this.http
+      .get('http://projet-angular/liste-produit.php')
+      .subscribe((listeProduit) => (this.listeProduit = listeProduit));
+  }
 }
--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatInputModule],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.scss',
 })
